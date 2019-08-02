25f2c95e78697ebadf95aa43df49883c811048e7
luke mackenzie <luke@sapho.co.uk>
2019-08-02 10:58:38 +0100

    wrapper for wordpress

---
+++ calendarTools.php
@@ -0,0 +1,17 @@
+
+<?php
+/*
+Plugin Name: Calendar Tools
+Plugin URI: https://github.com/hoofman/calendarTools
+Description: Calendar Tools For DreamSpell / LongCount (tzolkin / haab)
+Author: Luke Mackenzie - 13 earth - luke@sapho.co.uk
+Author URI: http://hoofman.sdf-eu.org/maya
+version: 1.00
+*/
+
+add_action('wp_enqueue_scripts','calendar_tools_init');
+
+function calendar_tools_init() {
+    wp_enqueue_script( 'testAll', plugins_url( '/js/calendarTools.js', __FILE__ ));
+}
+?>
